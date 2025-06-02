5f0e493eb9b81b3c216f38bbd8aa0464f0b1791b
amittima1234 <amittima1234@gmail.com>
2025-06-02 15:20:51 +0300

    added PWA capabilities

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: "autoUpdate",
+			includeAssets: [
+				"favicon.svg",
+				"favicon.ico",
+				"robots.txt",
+				"apple-touch-icon.png",
+			],
+			manifest: {
+				name: "Mishmarot",
+				short_name: "Mishmarot",
+				description: "Mishmarot is an app for managing shifts",
+				theme_color: "#ffffff",
+				icons: [
+					{
+						src: "web-app-manifest-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "web-app-manifest-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+				],
+			},
+		}),
+	],
+});
